c123a5e80bd77e15b1d46a53553c5f7822cc1409
Axsusia <zion437@gmail.com>
2014-07-22 22:15:10 +0900

    my pc

--- Cool.js
+++ Cool.js
@@ -1,34 +1,41 @@
+/* ===============================================================
+ * Object Add
+ * + prototype.method()
+ * + prototype.extend()
+ * + 
+ * =============================================================== */
 /**
- * 
+ * 프로토타입 확장 메소드 
+ *
+ * @author SeoSiwon
+ * @param 
  */
+Object.prototype.method = function () {
+
+}
+/**
+ * Object 프로토타입 확장
+ * @author SeoSiwon
+ * @param tagetObj
+ */
+Object.prototype.extend = function (targetObj) {
+	if (targetObj == null) return new Object();
+	var obj = new Object();
+	obj.prototype = targetObj.prototype;
+	return obj;
+}
+
+	Object.create = Object.create || function(o){
+	var F = function(){};
+	F.prototype = o;
+	return new F();
+}
+
+Function.prototype.method = function(name, func){
+	this.prototype[name] = func;
+	return this;
+}
 (function (window, document) {
-
-	/* ===============================================================
-	 * Object Add
-	 * + prototype.method()
-	 * + prototype.extend()
-	 * + 
-	 * =============================================================== */
-	/**
-	 * 프로토타입 확장 메소드 
-	 *
-	 * @author SeoSiwon
-	 * @param 
-	 */
-	Object.prototype.method = function () {
-
-	}
-	/**
-	 * Object 프로토타입 확장
-	 * @author SeoSiwon
-	 * @param tagetObj
-	 */
-	Object.prototype.extend = function (targetObj) {
-		if (targetObj == null) return new Object();
-		var obj = new Object();
-		obj.prototype = targetObj.prototype;
-		return obj;
-	}
 
 	/* ===============================================================
 	 * Element Add
